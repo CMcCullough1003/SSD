7923604421ed3d40f472cb065efe482b89e1f197
Cameron McCullough <cmcc1003@gmail.com>
2019-09-02 21:50:17 +0100

    initial commit

---
+++ Week1Practicals/Exercise1/Exercise1/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1
+{
+    public class Names
+    {
+        public Names()
+        {
+
+        }
+
+        public void PrintName()
+        {
+            string name = "";
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", name);
+        }
+ 
+    }
+}
