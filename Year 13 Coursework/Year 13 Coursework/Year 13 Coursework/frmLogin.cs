253b849c748bc9a9912ea9e3dbdda00b354702f4
Cameron McCullough <cmcc1003@gmail.com>
2019-10-15 20:52:40 +0100

    Year 13 project updates

---
+++ Year 13 Coursework/Year 13 Coursework/Year 13 Coursework/frmLogin.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Year_13_Coursework
+{
+    public partial class frmLogin : Form
+    {
+        private const string USER_FILENAME = "Users.txt";
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string usernameInput = tbxUsername.Text;
+            string passwordInput = tbxPassword.Text;
+
+            if (checkInputsProvided(usernameInput, passwordInput))
+            {
+                MessageBox.Show("Username and Password must not be empty", "try again");
+                return;
+            }
+
+            if (!userFileExists())
+            {
+                MessageBox.Show("Please check the file exists", "No file found");
+                return;
+            }
+
+            List<string> users = getUsers();
+
+            foreach(string element in users)
+            {
+                List<string> userDetails = element.Split('~').ToList();
+
+                for(int i = 0; i<userDetails.Count; i++)
+                {
+                    string usernameFile = userDetails[0];
+                    string passwordFile = userDetails[1];
+                    
+                    if(usernameFile == usernameInput && passwordFile == passwordInput)
+                    {
+                        moveToStartScreen();
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Your username or password is incorrect", "failure");
+                        clearUserInputs();
+                        break;
+                    }             
+                }
+            }
+        }
+
+        private void clearUserInputs()
+        {
+            tbxPassword.Clear();
+            tbxUsername.Clear();
+        }
+
+        private Boolean userFileExists()
+        {
+            return File.Exists(USER_FILENAME);
+        }
+
+        private List<string> getUsers()
+        {
+            return File.ReadLines(USER_FILENAME).ToList();
+        }
+
+        private Boolean checkInputsProvided(string name, string password) 
+        {
+            return string.IsNullOrEmpty(name) || string.IsNullOrEmpty(password);
+        }
+
+        private void moveToStartScreen()
+        {
+            this.Hide();
+            Form MoveToStartScreen = new frmStartScreen();
+            MoveToStartScreen.Show();
+        }
+
+        private void moveToRegisterScreen()
+        {
+            this.Hide();
+            Form MoveToRegister = new frmRegister();
+            MoveToRegister.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moveToRegisterScreen();
+        }
+    }
+}
---
+++ Year 13 Coursework/Year 13 Coursework/Year 13 Coursework/frmRegister.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Year_13_Coursework
+{
+    partial class frmRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.lblAvatar = new System.Windows.Forms.Label();
+            this.tbxConfirmPassword = new System.Windows.Forms.TextBox();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.tbxUsername = new System.Windows.Forms.TextBox();
+            this.cbxAvatar = new System.Windows.Forms.ComboBox();
+            this.btnBackToLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(204, 23);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(166, 51);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Register";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(164, 94);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(114, 27);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(164, 140);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(110, 27);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmPassword.Location = new System.Drawing.Point(164, 187);
+            this.lblConfirmPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(196, 27);
+            this.lblConfirmPassword.TabIndex = 3;
+            this.lblConfirmPassword.Text = "Confirm Password:";
+            // 
+            // lblAvatar
+            // 
+            this.lblAvatar.AutoSize = true;
+            this.lblAvatar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvatar.Location = new System.Drawing.Point(164, 229);
+            this.lblAvatar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAvatar.Name = "lblAvatar";
+            this.lblAvatar.Size = new System.Drawing.Size(82, 27);
+            this.lblAvatar.TabIndex = 4;
+            this.lblAvatar.Text = "Avatar:";
+            // 
+            // tbxConfirmPassword
+            // 
+            this.tbxConfirmPassword.Location = new System.Drawing.Point(379, 187);
+            this.tbxConfirmPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbxConfirmPassword.Name = "tbxConfirmPassword";
+            this.tbxConfirmPassword.Size = new System.Drawing.Size(212, 22);
+            this.tbxConfirmPassword.TabIndex = 5;
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(379, 140);
+            this.tbxPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(212, 22);
+            this.tbxPassword.TabIndex = 6;
+            // 
+            // tbxUsername
+            // 
+            this.tbxUsername.Location = new System.Drawing.Point(379, 95);
+            this.tbxUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbxUsername.Name = "tbxUsername";
+            this.tbxUsername.Size = new System.Drawing.Size(212, 22);
+            this.tbxUsername.TabIndex = 7;
+            // 
+            // cbxAvatar
+            // 
+            this.cbxAvatar.FormattingEnabled = true;
+            this.cbxAvatar.Location = new System.Drawing.Point(379, 229);
+            this.cbxAvatar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxAvatar.Name = "cbxAvatar";
+            this.cbxAvatar.Size = new System.Drawing.Size(160, 24);
+            this.cbxAvatar.TabIndex = 8;
+            // 
+            // btnBackToLogin
+            // 
+            this.btnBackToLogin.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnBackToLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackToLogin.ForeColor = System.Drawing.Color.White;
+            this.btnBackToLogin.Location = new System.Drawing.Point(169, 390);
+            this.btnBackToLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBackToLogin.Name = "btnBackToLogin";
+            this.btnBackToLogin.Size = new System.Drawing.Size(156, 49);
+            this.btnBackToLogin.TabIndex = 10;
+            this.btnBackToLogin.Text = "Back";
+            this.btnBackToLogin.UseVisualStyleBackColor = false;
+            this.btnBackToLogin.Click += new System.EventHandler(this.btnBackToLogin_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(169, 260);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(201, 49);
+            this.btnRegister.TabIndex = 11;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 452);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnBackToLogin);
+            this.Controls.Add(this.cbxAvatar);
+            this.Controls.Add(this.tbxUsername);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.tbxConfirmPassword);
+            this.Controls.Add(this.lblAvatar);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmRegister";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmRegister";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.Label lblAvatar;
+        private System.Windows.Forms.TextBox tbxConfirmPassword;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.TextBox tbxUsername;
+        private System.Windows.Forms.ComboBox cbxAvatar;
+        private System.Windows.Forms.Button btnBackToLogin;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
