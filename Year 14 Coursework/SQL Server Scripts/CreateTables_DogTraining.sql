7d09c19663fb2ab1c77174b2c66aa03d0a11777e
Cameron McCullough <cmcc1003@gmail.com>
2020-07-15 17:55:11 +0100

    Check constraints

--- Year 14 Coursework/SQL Server Scripts/CreateTables_DogTraining.sql
+++ Year 14 Coursework/SQL Server Scripts/CreateTables_DogTraining.sql
@@ -19,9 +19,10 @@ DROP TABLE Client;
 
 CREATE TABLE Client (
 	ClientID int IDENTITY(1,1) PRIMARY KEY,
-	Name varchar(100) NOT NULL,
-	Phone varchar(20),
-	Email varchar(50)
+	Name varchar(100) CHECK (Len(Name) >= 3) NOT NULL,
+	Phone varchar(20) CHECK (Len(Phone) >= 10),
+	Email varchar(50) CHECK (Len(Email) >= 7),
+	CHECK (Phone IS NOT NULL OR Email IS NOT NULL)
 )
 
 CREATE TABLE Dog (
@@ -34,38 +35,37 @@ CREATE TABLE Dog (
 
 CREATE TABLE Staff (
 	StaffID int IDENTITY(1,1) PRIMARY KEY,
-	Name varchar(100) NOT NULL
+	Name varchar(100) CHECK (Len(Name) >= 3) NOT NULL
 ) 
 
 CREATE TABLE ProgramType (
 	ProgramTypeID int IDENTITY(1,1) PRIMARY KEY,
-	Description varchar(20) NOT NULL
+	Description varchar(20) CHECK (Description in ('Regular','Advanced')) NOT NULL
 )
 
 CREATE TABLE ProgramCost (
 	ProgramCostID int IDENTITY(1,1) PRIMARY KEY,
-	DepositAmount float NOT NULL,
-	SessionCost float NOT NULL,
-	FullPaymnetPercentageDiscount float NOT NULL
+	DepositAmount float CHECK (DepositAmount >= 20.0 AND DepositAmount <= 100.0) NOT NULL,
+	SessionCost float CHECK (SessionCost >= 20.0 AND SessionCost <= 100.0) NOT NULL,
+	FullPaymentPercentageDiscount float CHECK (FullPaymentPercentageDiscount >= 0 AND FullPaymentPercentageDiscount <= 100) NOT NULL
 ) 
 
 CREATE TABLE Program (
 	ProgramID int IDENTITY(1,1) PRIMARY KEY,
 	ProgramTypeID int FOREIGN KEY REFERENCES ProgramType(ProgramTypeID),
 	ProgramCostID int FOREIGN KEY REFERENCES ProgramCost(ProgramCostID),
-	DogSpacesMaximum int NOT NULL,
-	NoOfClasses int NOT NULL
+	DogSpacesMaximum int CHECK (DogSpacesMaximum >= 1 AND DogSpacesMaximum <= 100) NOT NULL,
+	NoOfClasses int CHECK (NoOfClasses >= 1 AND NoOfClasses <= 100) NOT NULL
 )
 
 CREATE TABLE Class (
 	ClassID int IDENTITY(1,1) PRIMARY KEY,
 	ProgramID int FOREIGN KEY REFERENCES Program(ProgramID),
 	StaffID int FOREIGN KEY REFERENCES Staff(StaffID),
-	Date date NOT NULL,
+	Date date CHECK (Date >= GetDate()) NOT NULL,
 	StartTime time NOT NULL,
 	EndTime time NOT NULL
 )
-
 
 CREATE TABLE ClientDog (
 	ClientID int FOREIGN KEY REFERENCES Client(ClientID),
@@ -83,7 +83,7 @@ CREATE TABLE Enrollment (
 CREATE TABLE Payment (
 	PaymentID int IDENTITY(1,1) PRIMARY KEY,
 	EnrollmentID int FOREIGN KEY REFERENCES Enrollment(EnrollmentID),
-	PaymentAmountDue float NOT NULL,
+	PaymentAmountDue float CHECK (PaymentAmountDue >= 0.0) NOT NULL,
 	PaymentAmountDueDate date NOT NULL,
 	PaymentRecieved bit DEFAULT 0 NOT NULL,
 	PaymentRecievedDate date,
