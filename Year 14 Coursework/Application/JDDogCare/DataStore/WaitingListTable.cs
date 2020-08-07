57d9020238b7e61e1ab099fe2594bff8098e4422
Cameron McCullough <cmcc1003@gmail.com>
2020-08-07 20:25:58 +0100

    Delete All

---
+++ Year 14 Coursework/Application/JDDogCare/DataStore/WaitingListTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataStore
+{
+    class WaitingListTable
+    {
+        public void deleteAll()
+        {
+            try
+            {
+                DataStoreHelper dataStoreHelper = new DataStoreHelper();
+                SqlConnection connection = dataStoreHelper.createConnection();
+
+                SqlCommand commandCreate = new SqlCommand("DeleteWaitingListAll");
+                commandCreate.CommandType = System.Data.CommandType.StoredProcedure;
+
+                commandCreate.Connection = connection;
+
+                commandCreate.ExecuteNonQuery();
+
+                dataStoreHelper.closeConnection(connection);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception thrown: " + ex.Message);
+            }
+        }
+    }
+}
